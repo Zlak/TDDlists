40717bb15d6d93bf3884fff6fe234d45f2ee92d3
Lysenko Alexey <nachinka@mail.ru>
2017-07-10 00:11:11 +1000

    first model for Items +migrations

--- superlists/functional_test.py
+++ superlists/functional_test.py
@@ -2,11 +2,12 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
 
+import time
 
 class NewVisitorTest(unittest.TestCase):
     def setUp(self):
         self.browser = webdriver.Firefox(executable_path=r'd:\Users\Z&B\PycharmProjects\geckodriver.exe')
-        # self.browser.implicitly_wait(3)
+        self.browser.implicitly_wait(3)
     def tearDown(self):
         self.browser.quit()
 
@@ -33,17 +34,25 @@ class NewVisitorTest(unittest.TestCase):
 
         # page updates on ENTER
         inputbox.send_keys(Keys.ENTER)
+        self.browser.implicitly_wait(1)
+       # time.sleep(10)
 
         # now page lists '1. Buy peacock feathers'
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(any(row.text == '1: buy peacock feathers' for row in rows))
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
 
         # there is still invitation to enter next to-do item
-
-        # entered 'Use peacock feathers to nake a fly'
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        # entered 'Use peacock feathers to make a fly'
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+        self.browser.implicitly_wait(1)
         # page updates and shows two items
-
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
         # this list have unique URL to be accessed later
         # check this URL
         self.fail('Finish the test')
