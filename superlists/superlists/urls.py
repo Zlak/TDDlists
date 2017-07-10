e3012ba78b094574c3173ab0089660b208759e20
Lysenko Alexey <nachinka@mail.ru>
2017-07-10 20:04:58 +1000

    started multiple list realisation new view for list home page now only for new list

--- superlists/superlists/urls.py
+++ superlists/superlists/urls.py
@@ -18,6 +18,8 @@ from django.contrib import admin
 from lists import views
 
 urlpatterns = [
-    url(r'^$', views.home_page, name = 'home'),
+    url(r'^$', views.home_page, name='home'),
     url(r'^admin/', admin.site.urls),
+    url(r'^lists/unique-list/$', views.view_list, name='view_list')
+
 ]
